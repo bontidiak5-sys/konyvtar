f8302dfe75f81a12039ba999ba155e6dda5720ef
Takács Zsolt <takacszsolt2007@gmail.com>
2026-01-21 13:07:45 +0100

    jskezdo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+function availableBooks(books) {
+    return books.filter(function(book) {
+        return !book.kolcsonozve;
+    });
+}
+function borrowedBooks(books) {
+    return books.filter(function(book) {
+        return book.kolcsonozve;
+    });
+}
+function filterByAuthor(books, author) {
+    return books.filter(function(book) {
+        return book.szerzo.toLowerCase().includes(author.toLowerCase());
+    });
+}
 
